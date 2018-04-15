baabe61a7f36f56614d331db10849a8bd8a4f10c
Daniel Madrigal <danieltristan1991@gmail.com>
2018-04-15 16:04:43 +0200

    Commit

---
+++ ConsoleApp1/Singleton.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    //Estas dos interface son incompatibles dentro de la misma clase ya que comparten metodos con el mismo nombre
+    public interface IEjemplo
+    {
+        int Suma();
+        int Resta();
+    }
+    public interface Iejemplo2
+    {
+        int Suma(int x);
+        int Resta(int y);
+
+    }
+    public class Calculo : Iejemplo2
+    {
+        
+        public int Resta(int x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Suma(int y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Adapter : IEjemplo 
+    {
+        Iejemplo2 iejemplo2;
+        int x;
+        public Adapter()
+        {
+            iejemplo2 = new Calculo();
+        }
+        public int Resta()
+        {
+            return iejemplo2.Resta(x);
+        }
+
+        public int Suma()
+        {
+            return iejemplo2.Resta(x);
+        }
+    }
+}
